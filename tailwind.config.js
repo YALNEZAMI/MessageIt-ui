50fb6cf38b9b7b7555f634261fd4a9c2533e3e2a
YALNEZAMI <yaseralnezami@gmail.com>
2024-10-07 01:41:25 +0200

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Specify the paths to all of the templates and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
