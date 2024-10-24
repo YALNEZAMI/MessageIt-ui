a02317754ad09d85c5fc1c374561bbf4cbb19e1a
YALNEZAMI <yaseralnezami@gmail.com>
2024-10-24 22:34:51 +0200

    refact: make some properties optional to not init it on use

--- src/app/Interfaces/User.interface.ts
+++ src/app/Interfaces/User.interface.ts
@@ -2,9 +2,9 @@ export interface User {
   _id: string;
   firstName: string;
   lastName: string;
-  email: string;
-  password: string;
-  password2: string;
+  email?: string;
+  password?: string;
+  password2?: string;
   photo: string;
-  operation: string; //add,remove,accepte,refuse
+  operation?: string; //add,remove,accepte,refuse
 }
